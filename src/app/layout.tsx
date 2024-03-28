3b4d357c5d0d170b0c3d4f8fcb1a099afcfa177a
Shunki <tajimura0505@gmail.com>
2024-03-28 19:54:40 +0900

    metaタグの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,35 @@ import { CssBaseline } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "寿の店",
-  description: "名古屋市熱田区にある寿の店のHPです。",
+  description:
+    "名古屋市熱田区にある「寿の店」の公式HPです。寿の店は、先代から受け継ぐ鉄板プレス機を使ってその場で焼いた、ぺらっとパリッとしたせんべい、ピリ辛のイカ焼きを提供しています。",
+  openGraph: {
+    title: "寿の店 公式HP",
+    description: "名古屋市熱田区にある「寿の店」の公式HPです。",
+    url: "https://taihoukotobuki.vercel.app/",
+    siteName: "寿の店 公式HP",
+    images: [
+      {
+        url: "https://taihoukotobuki.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FDSC00133.b6570d4c.jpg&w=1080&q=75", // Must be an absolute URL
+        width: 500,
+        height: 500,
+      },
+      {
+        url: "https://taihoukotobuki.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FDSC00133.b6570d4c.jpg&w=1080&q=75", // Must be an absolute URL
+        width: 1000,
+        height: 1000,
+        alt: "寿の店の店頭写真",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "寿の店 公式HP",
+    description: "名古屋市熱田区にある「寿の店」の公式HPです。",
+    creator: "@ojira0808",
+  },
 };
 
 export default function RootLayout({
